f05430bea04165ce1fc2a7b8ba4201b6d58361c6
pdhub <pritom72@gmail.com>
2022-10-14 21:24:43 +0530

    all pickle files added

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from typing import Optional
 
 from fastapi import FastAPI, Request
-
+import numpy as np
+import pickle
+popular_df = pickle.load(open('popular.pkl','rb'))
+books = pickle.load(open('books.pkl','rb'))
+pt = pickle.load(open('pt.pkl','rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
 app = FastAPI()
 
 @app.get('/')
@@ -16,4 +21,14 @@ def read_item(id: int):
 
 @app.get("/recommend/")
 def recommend(bookName: Optional[str]=None):
-    return bookName;
+    index = np.where(pt.index==bookName)[0][0]
+    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x:x[1],reverse=True)[1:6]
+    data = []
+    for i in similar_items:
+        item = []
+        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author']))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M']))
+        data.append(item)
+    return data
